935034b9de2529e1f87fccefced45c2766dc7e75
Jacek Swierk <jacek.swierk@nokia.com>
2020-12-06 10:38:20 +0100

    delta: add package

---
+++ install-delta.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+pkgname=delta
+source="git+https://github.com/dandavison/${pkgname}.git"
+do_builddir=0
+
+do_configure() {
+  :
+}
+
+do_compile() {
+  cd "${srcdir}"
+  cargo build --release --locked
+}
+
+do_install() {
+  cd "${srcdir}"
+  install -dm 755 "${pkgdir}/bin"
+  install -Dm 755 "target/release/${pkgname}" -t "${pkgdir}/bin"
+}
+
+if [ -n "${BASH_SOURCE}" ]; then
+  this_script=${BASH_SOURCE}
+  this_dir="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
+elif [ -n "${ZSH_NAME}" ]; then
+  this_script=$0
+  this_dir="$(cd "$(dirname "$0")" && pwd)"
+else
+  echo "Error: Unsupported shell"
+  return 1
+fi
+
+source "${this_dir}/linsee-makepkg.sh"
+do_everything
